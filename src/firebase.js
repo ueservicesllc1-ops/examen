087bd75660bc56ac2ddf001c99da57c7fbeb15fc
Freedom Labs <freedom@labs.dev>
2026-03-16 12:40:32 -0400

    Refactor: Move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDRuaVNS5WaMWfvD9W3vnaARZqjpA3wM90",
-  authDomain: "examen-fd3cc.firebaseapp.com",
-  projectId: "examen-fd3cc",
-  storageBucket: "examen-fd3cc.firebasestorage.app",
-  messagingSenderId: "359459369807",
-  appId: "1:359459369807:web:612daa7258ea2aac18cb3a",
-  measurementId: "G-YVBJ7DPEXV"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
